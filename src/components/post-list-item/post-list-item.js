450a5b10a6e1b67ca258ca231515b8d216c6179a
Yurii <oliinyk0590@gmail.com>
2019-06-13 20:35:00 +0300

    Lessons5 - Second exercise

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './post-list-item.css';
 
 export default class PostListItem extends Component {
@@ -6,10 +7,12 @@ export default class PostListItem extends Component {
     super(props);
     this.state = {
       important: false,
-      like: false
+      like: false,
+      modal: false
     };
     this.onImportant = this.onImportant.bind(this);
     this.onLike = this.onLike.bind(this);
+    this.toggle = this.toggle.bind(this);
   }
 
   onImportant(){
@@ -20,6 +23,11 @@ export default class PostListItem extends Component {
   onLike(){
     this.setState(({like}) => ({
       like: !like
+    }));
+  }
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal
     }));
   }
   render() {
@@ -47,9 +55,20 @@ export default class PostListItem extends Component {
         </button>
         <button 
           type="button" 
-          className="btn-trash btn-sm" onClick={onDelete}>
+          className="btn-trash btn-sm"
+          onClick={this.toggle}>
           <i className="fa fa-trash-o"></i>
         </button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>Подтверждение</ModalHeader>
+          <ModalBody>
+            Вы точно собираетесь удалить запись ?
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={onDelete}>Да</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Нет</Button>
+          </ModalFooter>
+        </Modal>
         <i className="fa fa-heart"></i>
       </div>
     </div>
